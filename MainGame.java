2c441a97d6d577bdb6c30f96d91c744a5e1370b8
NoisyDv <tnnaphwngwili@gmail.com>
2025-03-06 18:29:14 +0700

    add potion

--- MainGame.java
+++ MainGame.java
@@ -14,16 +14,15 @@ public class MainGame extends JPanel implements ActionListener, KeyListener {
   Tile floor;
   BufferedImage map;
   Enemy enemy;
-  Random random=new Random();
+  Random random = new Random();
   Coin coin;
-  int score=0;
+  int score = 0;
   JLabel scoretext;
-  JLabel gameover=new JLabel();
-  JButton play_again=new JButton("Play Again");
-
-  
+  JLabel gameover = new JLabel();
+  JButton play_again = new JButton("Play Again");
+  Potion potion;
+  boolean spawn_potion;
   int velocity = 50;
-  
 
   // create panel by constructor
   MainGame(int width, int height) {
@@ -34,39 +33,35 @@ public class MainGame extends JPanel implements ActionListener, KeyListener {
     setBackground(Color.BLACK);
     addKeyListener(this);
     setFocusable(true);
-    //start gameloop
+    // start gameloop
     gameloop = new Timer(16, this);
     gameloop.start();
 
+    // make gameobject
     player = new Player(5 * Tile.Tile_size, 5 * Tile.Tile_size);
-    enemy = new Enemy(random.nextInt(width-Tile.Tile_size),random.nextInt(height-Tile.Tile_size));
+    enemy = new Enemy(random.nextInt(width - Tile.Tile_size), random.nextInt(height - Tile.Tile_size));
     floor = new Tile(floorImg, 0, 0);
-    coin=new Coin(random.nextInt(width-Tile.Tile_size),random.nextInt(height-Tile.Tile_size));
+    coin = new Coin(random.nextInt(width - Tile.Tile_size), random.nextInt(height - Tile.Tile_size));
+    potion = new Potion(random.nextInt(width - Tile.Tile_size), random.nextInt(height - Tile.Tile_size));
+    // make game over Label
+    gameover.setText("Game Over \n your score is " + score);
+    gameover.setForeground(Color.RED);
+    gameover.setBounds(450, 250, 500, 500);
+    gameover.setFont(new Font("FiraCode Neard Font", Font.BOLD, 32));
 
-     //make game over Label  
-     gameover.setText("Game Over \n your score is "+score);
-     gameover.setForeground(Color.RED);
-     gameover.setBounds(450,250,500,500);
-     gameover.setFont(new Font("FiraCode Neard Font",Font.BOLD,32));
-
-    //play again button   
-    play_again.setBounds(580,540,100,80);
+    // play again button
+    play_again.setBounds(580, 540, 100, 80);
     play_again.addActionListener(this);
-    
-    
-    
-    
-    
-    //make Score Label
-    scoretext=new JLabel("Score : "+score);
+
+    // make Score Label
+    scoretext = new JLabel("Score : " + score);
     scoretext.setForeground(Color.RED);
-    scoretext.setBounds(width-200,10,300,50);
-    scoretext.setFont(new Font("FiraCode Neard Font",Font.BOLD,30));
+    scoretext.setBounds(width - 200, 10, 300, 50);
+    scoretext.setFont(new Font("FiraCode Neard Font", Font.BOLD, 30));
     setLayout(null);
     add(scoretext);
-    //create Map
+    // create Map
     createMap();
-    
 
   }
 
@@ -86,18 +81,24 @@ public class MainGame extends JPanel implements ActionListener, KeyListener {
     // draw Player
     g.drawImage(player.playerImg, player.x, player.y, this);
 
-    //draw Enemy 
-    g.drawImage(enemy.enemyImg,enemy.x,enemy.y,this);
-   
-    //draw Coin
-    
-      g.drawImage(coin.coinImg,coin.x,coin.y,this);
+    // draw Enemy
+    g.drawImage(enemy.enemyImg, enemy.x, enemy.y, this);
 
-    //draw player HP
+    // draw Coin
+
+    g.drawImage(coin.coinImg, coin.x, coin.y, this);
+
+    // draw Potion
+    if (spawn_potion) {
+
+      g.drawImage(potion.potionImg, potion.x, potion.y, this);
+    }
+
+    // draw player HP
     g.setColor(Color.RED);
-    g.fillRect(0, 0,300,30);
+    g.fillRect(0, 0, 300, 30);
     g.setColor(Color.GREEN);
-    g.fillRect(0, 0,player.hp,30);
+    g.fillRect(0, 0, player.hp, 30);
   }
 
   // create Map
@@ -162,54 +163,62 @@ public class MainGame extends JPanel implements ActionListener, KeyListener {
   // manage event and gameloop
   @Override
   public void actionPerformed(ActionEvent e) {
-    //check collision player with enemey 
-   if(Tile.colliderRect(player,enemy)){
-    player.hp-=10;
-    System.out.println("colliderect!");
-   }
-   //check collision player with coin
-   if(Tile.colliderRect(player, coin)){
-    score++;
-    scoretext.setText("Score : "+score);
-    coin = new Coin(random.nextInt(width - Tile.Tile_size), random.nextInt(height - Tile.Tile_size));
-    enemy.Evelocity+=1;
-    System.out.printf(" score is %d velocity of enemy  %d \n" ,score,enemy.Evelocity);
-    
-   }
-  //enemy follow the player
-        enemy.Enemy_move(player.x, player.y);
-  
-  //game over condition
-  if(player.hp<=0){
-    gameloop.stop();
-    gameover.setText("Game Over \n your score is "+score);
-    setFocusable(false);
-    add(gameover);
-    add(play_again);
-    
-  }
-  //game restart 
-  if(e.getSource()==play_again){
-    remove(gameover);
-    remove(play_again);
-    score=0;
-    enemy.Evelocity=1;
-    scoretext.setText("Score: "+score);
-    player.hp=300;
-    velocity=50;
-    player.x=random.nextInt(width-Tile.Tile_size);
-    player.y=random.nextInt(height-Tile.Tile_size);
-    enemy.x=random.nextInt(width-Tile.Tile_size);
-    enemy.y=random.nextInt(height-Tile.Tile_size);
-  coin = new Coin(random.nextInt(width - Tile.Tile_size), random.nextInt(height - Tile.Tile_size)); 
-    setFocusable(true);
-    requestFocusInWindow();
-    gameloop.start();
-    
-    
+    // check collision player with enemey
+    if (Tile.colliderRect(player, enemy)) {
+      player.hp -= 2;
+      System.out.println("colliderect!");
+    }
+    // check collision player with coin
+    if (Tile.colliderRect(player, coin)) {
+      score++;
+      scoretext.setText("Score : " + score);
+      coin = new Coin(random.nextInt(width - Tile.Tile_size), random.nextInt(height - Tile.Tile_size));
+      enemy.Evelocity += 1;
+      System.out.printf(" score is %d velocity of enemy  %d \n", score, enemy.Evelocity);
+      if (potion.spawnChange(random.nextInt(101))) {
+        spawn_potion = true;
+      }
 
-  }
+    }
+    // check collision player with potion
+    if (Tile.colliderRect(player, potion)) {
+      potion = new Potion(random.nextInt(width - Tile.Tile_size), random.nextInt(height - Tile.Tile_size));
+      player.hp += potion.recovery;
+      spawn_potion = false;
+    }
+
+    // enemy follow the player
+    enemy.Enemy_move(player.x, player.y);
+
+    // game over condition
+    if (player.hp <= 0) {
+      gameloop.stop();
+      gameover.setText("Game Over \n your score is " + score);
+      setFocusable(false);
+      add(gameover);
+      add(play_again);
+
+    }
+    // game restart
+    if (e.getSource() == play_again) {
+      remove(gameover);
+      remove(play_again);
+      score = 0;
+      enemy.Evelocity = 1;
+      scoretext.setText("Score: " + score);
+      player.hp = 300;
+      velocity = 50;
+      player.x = random.nextInt(width - Tile.Tile_size);
+      player.y = random.nextInt(height - Tile.Tile_size);
+      enemy.x = random.nextInt(width - Tile.Tile_size);
+      enemy.y = random.nextInt(height - Tile.Tile_size);
+      coin = new Coin(random.nextInt(width - Tile.Tile_size), random.nextInt(height - Tile.Tile_size));
+      setFocusable(true);
+      requestFocusInWindow();
+      gameloop.start();
+
+    }
+
     repaint();
-
   }
 }
--- Potion.java
+++ Potion.java
@@ -1,17 +1,30 @@
-public class Potion extends Item{
-    Potion(int x,int y){
-        super(x,y);
-        
+import java.util.Random;
 
-    }
-    @Override
-    boolean spawnChange(int change){
-            if(change>70){
-                return true;
-            }
-            return false;
-            
+import javax.swing.ImageIcon;
 
+import java.awt.*;
+
+public class Potion extends Item {
+  Random random = new Random();
+  Image potionImg = new ImageIcon("pl/potion1.gif").getImage();
+
+  int recovery = random.nextInt(40, 51);
+
+  Potion(int x, int y) {
+    super(x, y);
+    this.x = x;
+    this.y = y;
+    this.img = potionImg;
+
+  }
+
+  @Override
+  boolean spawnChange(int change) {
+    if (change > 60) {
+      return true;
     }
-    
+    return false;
+
+  }
+
 }
